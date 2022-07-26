2c730773c60b5bc325280a86993498c3a9f8f418
AnupOmm123 <anupamaxe94@yahoo.com>
2022-07-26 12:51:39 +0530

    1

---
+++ src/test/java/com/crm/mohabharat/Mahabharat.java
@@ -0,0 +1,15 @@
+package com.crm.mohabharat;
+
+public class Mahabharat {
+
+	public static void main(String[] args) {
+		System.out.println("mahabharat");
+		System.out.println("mahabharat32");
+System.out.println("no mahabharat");
+System.out.println("arjun");
+		System.out.println("bhim");
+		System.out.println("yudhistir");
+		System.out.println("nakul");
+	}
+
+}
